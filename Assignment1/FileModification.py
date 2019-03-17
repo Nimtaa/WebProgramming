84e9fd668b671a2aa07801deb4cbf183592d67d6
Nimtaa <nima.tavassoli2@gmail.com>
2019-03-18 00:16:45 +0330

    console interface added

--- Assignment1/FileModification.py
+++ Assignment1/FileModification.py
@@ -21,3 +21,29 @@ def addAddress(group, address):
     with open(filename, 'w') as f:
         json.dump(data, f)
     
+def main(argv):
+    group = ''
+    address = ''
+    try:
+        opts, args = getopt.getopt(argv,"hg:a:")
+    except getopt.GetoptError:
+        print 'FileModification.py -g <group> -a <address>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'Adding URL to the file: python FileModification.py -g <group> -a <address>' 
+            sys.exit()
+        elif opt in ("-g"):
+            group  = arg
+            # addGroup(group)
+        elif opt in ("-a"):
+            address = arg
+    if(group and address):
+        addAddress(group,address)
+    else:
+        addGroup(group)
+    print 'Group is "', group
+    print 'Address is "', address
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
