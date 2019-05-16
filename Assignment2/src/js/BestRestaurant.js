ccfca395728ea1bb8c53f3ba4028010f62a89312
Nimtaa <nima.tavassoli2@gmail.com>
2019-05-16 19:01:38 +0430

    js updated

--- Assignment2/src/js/BestRestaurant.js
+++ Assignment2/src/js/BestRestaurant.js
@@ -2,7 +2,6 @@
 
 $.getJSON('http://demo2469824.mockable.io/best-restaurants', function(data) {
     //data is the JSON string
-    
     RenderBestinMonth(data);
 });
 
@@ -11,7 +10,34 @@ function RenderBestinMonth(data){
     $(".top_restaurant_div").each(function(){
        $( this ).children('img').attr('src',data['restaurants'][index].imgUrl);
        $( this ).children('h2').html(data['restaurants'][index].name);
-       $( this ).children('#rateSpan').html("("+data['restaurants'][index].numOfRates+")");
+       $( this ).children('#top_restaurant_rateSpan').html("("+data['restaurants'][index].numOfRates+")");
+       $( this ).children('.orange_rating').html("&nbsp;"+data['restaurants'][index].rate);
+       $( this ).children('#top_restaurant_address').html(data['restaurants'][index].address);
+       $( this ).children('.orange_rating').before(RenderStars(data['restaurants'][index].rate));
        index ++;
     })
+}
+
+
+function RenderStars(rate){
+    var stars = new Array();
+    natural_rate = Math.floor(rate);
+    for (i=0;i<natural_rate;i++){
+        // create checked star
+        var span = $('<span />').addClass('fa fa-star checked');
+        stars.push(span);
+    }
+    if(rate - natural_rate){
+        var span = $('<span />').addClass('fa fa-star-half-full halfchecked');
+        stars.push(span);
+    }
+    var unchecked = 5 - Math.ceil(rate);
+    for(i=0;i<unchecked;i++){
+        //create unchecked star
+        var span = $('<span />').addClass('fa fa-star-o unchecked');
+        stars.push(span);
+    }
+
+    return stars;
+    
 }
