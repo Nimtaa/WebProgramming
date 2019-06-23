a199c20613768696786a053c08f17923e143ce0c
Nimtaa <nima.tavassoli2@gmail.com>
2019-06-23 19:04:55 +0430

    Jquery/Ajax assignment

---
+++ Assignment3/src/js/BestRestaurant.js
@@ -0,0 +1,85 @@
+
+
+$.getJSON('http://demo2469824.mockable.io/best-restaurants', function(data) {
+    //data is the JSON string
+    RenderBestinMonth(data);
+    RenderGoodRestaurants(data);
+});
+
+function RenderBestinMonth(data){
+    var index = 0;
+    $(".top_restaurant_div").each(function(){
+       $( this ).children('img').attr('src',data['restaurants'][index].imgUrl);
+       $( this ).children('h2').html(data['restaurants'][index].name);
+       $( this ).children('#top_restaurant_rateSpan').html("("+data['restaurants'][index].numOfRates+")");
+       $( this ).children('.orange_rating').html("&nbsp;"+data['restaurants'][index].rate);
+       $( this ).children('#top_restaurant_address').html(data['restaurants'][index].address);
+       $( this ).children('.orange_rating').before(RenderStars(data['restaurants'][index].rate));
+       $( this ).children('#top_restaurant_food').html(RenderFoods(data['restaurants'][index].foods));
+
+       index ++;
+    })
+}
+
+function RenderFoods(listOfFood){
+    var persianList = new Array();
+    for(let i = 0; i < listOfFood.length; i++){
+        switch(listOfFood[i]){
+            case "pizza":
+                persianList.push("پیتزا");
+                persianList.push("&bull;");
+                break;
+            case "fastfood":
+                persianList.push("قست‌فود");
+                persianList.push("&bull;");
+                break;
+            case "sandwich":
+                persianList.push("ساندویج");
+                persianList.push("&bull;");
+                break;
+            case "burger":
+                persianList.push("برگر");
+                persianList.push("&bull;");
+                break;
+        }
+    }
+    persianList.pop();
+    return persianList;
+}
+
+function RenderStars(rate){
+    var stars = new Array();
+    natural_rate = Math.floor(rate);
+    for (i=0;i<natural_rate;i++){
+        // create checked star
+        var span = $('<span />').addClass('fa fa-star checked');
+        stars.push(span);
+    }
+    if(rate - natural_rate){
+        var span = $('<span />').addClass('fa fa-star-half-full halfchecked');
+        stars.push(span);
+    }
+    var unchecked = 5 - Math.ceil(rate);
+    for(i=0;i<unchecked;i++){
+        //create unchecked star
+        var span = $('<span />').addClass('fa fa-star-o unchecked');
+        stars.push(span);
+    }
+
+    return stars;
+    
+}
+
+
+function RenderGoodRestaurants(data){
+    var index = 3;
+    $(".grid_container").children().each(function(){
+        $(this).children('input').attr('src',data['restaurants'][index].imgUrl);
+        $(this).children('input').after(data['restaurants'][index].name);
+        index++;
+
+    });
+    
+    
+
+}
---
+++ Assignment3/src/js/FoodList.js
@@ -0,0 +1,74 @@
+$(function() {
+    $.ajax({
+        type: "get",
+        url: "http://demo2469824.mockable.io/foods",
+        dataType: "xml",
+        success: function(data) {
+            foodList = data;
+            RenderSection5_Grid_Container(data);
+            $('.more_foods').css('margin-left','215px');
+            $('.more_foods').css('margin-right','215px');
+            RenderMoreFoods(data);
+        },
+        error: function(xhr, status) {
+            /* handle error here */
+        }
+    });
+});
+
+function RenderSection5_Grid_Container(data){
+    var index = 3;
+    $(".section5_grid_container").children().each(function(){
+        var item = data.documentElement.getElementsByTagName("food")[index];
+        $(this).children(".section5_text").children('p').html(item.firstElementChild.innerHTML + " رستوران فعال");
+        $(this).children(".section5_text").children('h1').html(RenderNames(item.lastElementChild.innerHTML));
+        // $(this).children("#sec5_input").css('background','url(' + data.documentElement.getElementsByTagName("imgUrl")[index] + ')');
+        $(this).children("#sec5_input").css('background', "url(" + data.documentElement.getElementsByTagName("imgUrl")[index].innerHTML + ")");
+        $(this).children("#sec5_input").css('background-size', "cover");
+        if(index==3){
+            $(this).children("#sec5_input").css('background-blend-mode', "darken");
+        }
+        index--;
+    });
+}
+
+function RenderNames(data){
+    switch(data){
+        case "kebab":
+            return "کباب";
+        case "pizza":
+            return "پیتزا";
+        case "burger":
+            return "برگر";
+        case "sandwich":
+            return "ساندویچ";
+        case "iranian":
+            return "ایرانی";
+        case "soup":
+            return "سوپ";
+        case "steak":
+            return "استیک";
+        case "fish":
+            return "ماهی";
+        case "juice":
+            return "آبمیوه طبیعی";                
+        case "breakfast":
+            return "صبحانه";        
+        case "salad":
+            return "سالاد";
+        case "pasta":
+            return "پاستا";
+        
+        
+            
+        
+        
+    }
+}
+function RenderMoreFoods(data){
+    var index = 0;
+    $('.more_foods').children().each(function(){
+        $(this).html(RenderNames(data.documentElement.getElementsByTagName("name")[index].innerHTML));
+        index ++;
+    });
+}
